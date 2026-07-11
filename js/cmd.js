b6b3d23ed3143b3583acb7d8a89e27c6655b8d76
nokira <mattiadagosta0@gmail.com>
2026-07-11 22:39:00 +0200

    Made a simple cmd!
    Nothin much, more commands are coming soon

---
+++ js/cmd.js
@@ -0,0 +1,52 @@
+// PawCmd Code
+
+const pawCmdScreen = document.getElementById('pawCmd');
+const pawCmdScreenClose = document.getElementById('pawCmdClose');
+
+const cmdInput = document.getElementById('pawCmdInput');
+const prompt = document.getElementById('prompt');
+const cmdOutput = document.getElementById('pawCmdOutput');
+const pawCmdContent = document.getElementById('pawCmdContent');
+
+prompt.innerHTML = user + "@kittenOs $>";
+
+pawCmdContent.addEventListener("click", () => {
+    cmdInput.focus();
+});
+
+cmdInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+
+        const commandText = cmdInput.textContent.trim();
+        if (commandText == "") return;
+
+        const command = commandText.toLocaleLowerCase();
+
+        switch(command) {
+            case "help":
+                printTerminal("Hi!! Here are all commands:\nhelp\nhello\ncat\nclear");
+                break;
+            case "hello":
+                printTerminal("Hello! Welcome to PawCmd :)");
+                break;
+            case "cat":
+                printTerminal("(=^.^=) .... meow");
+                break;
+            case "clear":
+                history.innerHTML = "";
+            default:
+                printTerminal(`Command - ${command} - not found! Type help for available commands`);
+        }
+
+        cmdInput.textContent = "";
+    }
+});
+
+function printTerminal(cmd) {
+    cmdOutput.innerHTML = cmd;
+}
+
+pawCmdScreenClose.addEventListener("click", () => closeWindow(pawCmdScreen));
+
+initializeWindow("pawCmd");
